6cf501fc6aa70f849f33af33081611c02ec16ca6
seconrg <37467100+seconrg@users.noreply.github.com>
2018-05-12 09:08:58 +0800

    Update oshfs.c

--- oshfs.c
+++ oshfs.c
@@ -149,7 +149,7 @@ static int oshfs_write(const char *path, const char *buf, size_t size, off_t off
     }
                                     // now there is one
     node->st.st_size = offset + size;                              //修改文件的大小标志
-    int count=(size+offset)/BLOCK+1;                                     //the number of blocks that is needed,[(size+offset)/BLOCK]+1
+    int count=(size+offset-1)/BLOCK+1;                                     //the number of blocks that is needed,[(size+offset)/BLOCK]+1
     time(&rawtime);
     node->st.st_ctime=rawtime;
     node->st.st_mtime=rawtime;
@@ -199,9 +199,9 @@ static int oshfs_write(const char *path, const char *buf, size_t size, off_t off
             memcpy((char*)mem[node->content[i]], buf+off, remain);
             node->begin=remain;
             //printf("OK!\n");
-            //for (p=i+1;p<node->filelen;p++)
-              //  deleteamem(p);
-            //node->filelen=i+1;
+            for (p=i+1;p<node->filelen;p++)
+                deleteamem(node->content[p]);
+            node->filelen=i+1;
             return size;                            //work finished num>=count
         }
     }
